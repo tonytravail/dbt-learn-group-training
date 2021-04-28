5f5a5b939c3a654fdbe9a57b26d8e6e222740bf0
Christine Berger <christine@fishtownanalytics.com>
2021-04-28 22:21:15 +0000

    5.2 Intermediate Models or CTEs

--- models/marts/fct_customer_orders.sql
+++ models/marts/fct_customer_orders.sql
@@ -2,19 +2,13 @@ with
 
 orders as (
 
-  select * from {{ ref('stg_jaffle_shop__orders') }}
+  select * from {{ ref('int_orders') }}
 
 ),
 
 customers as (
 
   select * from {{ ref('stg_jaffle_shop__customers') }}
-
-),
-
-payments as (
-
-  select * from {{ ref('stg_stripe__payments') }}
 
 ),
 
@@ -26,36 +20,31 @@ customer_order_history as (
       customers.full_name,
       customers.surname,
       customers.givenname,
-      min(order_date) as first_order_date,
 
-      min(case 
-        when orders.order_status not in ('returned','return_pending') 
-        then order_date 
-      end) as first_non_returned_order_date,
+      min(orders.order_date) as first_order_date,
 
-      max(case 
-        when orders.order_status not in ('returned','return_pending') 
-        then order_date 
-      end) as most_recent_non_returned_order_date,
+      min(orders.valid_order_date) as first_non_returned_order_date,
 
-      coalesce(max(user_order_seq),0) as order_count,
+      max(orders.valid_order_date) as most_recent_non_returned_order_date,
+
+      coalesce(max(orders.user_order_seq),0) as order_count,
 
       coalesce(count(case 
-        when orders.order_status != 'returned' 
+        when orders.valid_order_date is not null
         then 1 
       end),0) as non_returned_order_count,
 
       sum(case 
-        when orders.order_status not in ('returned','return_pending') 
-        then payments.payment_amount else 0 
+        when orders.valid_order_date is not null
+        then orders.order_value_dollars else 0 
       end) as total_lifetime_value,
 
       sum(case 
-        when orders.order_status not in ('returned','return_pending') 
-        then payments.payment_amount else 0 
+        when orders.valid_order_date is not null
+        then orders.order_value_dollars else 0 
       end)
       / nullif(count(case 
-        when orders.order_status not in ('returned','return_pending') 
+        when orders.valid_order_date is not null
         then 1 
       end),0) as avg_non_returned_order_value,
       
@@ -65,11 +54,6 @@ customer_order_history as (
 
   join customers
   on orders.customer_id = customers.customer_id
-
-  left outer join payments
-  on orders.order_id = payments.order_id
-
-  where orders.order_status not in ('pending') and payments.payment_status != 'fail'
 
   group by customers.customer_id, customers.full_name, customers.surname, customers.givenname
 
@@ -86,9 +70,9 @@ final as (
       first_order_date,
       order_count,
       total_lifetime_value,
-      payment_amount as order_value_dollars,
+      orders.order_value_dollars,
       orders.order_status,
-      payments.payment_status
+      orders.payment_status
 
   from orders
 
@@ -97,11 +81,6 @@ final as (
 
   join customer_order_history
   on orders.customer_id = customer_order_history.customer_id
-
-  left outer join payments
-  on orders.order_id = payments.order_id
-
-  where payments.payment_status != 'fail'
 
 )
 
