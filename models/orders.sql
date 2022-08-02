1ee4c4fa6d89f1614072d004a4246a74276da97c
Logan Cochran <logan.cochran@fishtownanalytics.com>
2022-08-02 16:01:24 +0000

    update models to reflect style-guide

--- models/orders.sql
+++ models/orders.sql
@@ -1,12 +1,19 @@
-with orders as  (
+with 
+
+orders as (
+
     select * from {{ ref('stg_orders' )}}
+
 ),
 
 payments as (
+
     select * from {{ ref('stg_payments') }}
+    
 ),
 
 order_payments as (
+
     select
         order_id,
         sum(case when status = 'success' then amount end) as amount
@@ -25,6 +32,7 @@ final as (
 
     from orders
     left join order_payments using (order_id)
+    
 )
 
 select * from final
